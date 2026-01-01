fae536a4c89094986ab375549e64894e541eb064
Fazlul Hoque <sawravm299@gmail.com>
2026-01-01 22:01:54 +0600

    work order job update

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -12,6 +12,7 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule): void
     {
+        $schedule->command('queue:work --stop-when-empty')->everyMinute()->withoutOverlapping();
         $schedule->command('app:send-work-request-update')->everyMinute();
         $schedule->command('app:counter-offer-update')->everyMinute();
         $schedule->command('payments:update-status')->daily();
