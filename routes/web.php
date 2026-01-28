871121ca459bf1f4efa296452452a8eeb2d46a5b
Nahid Hasan Tamim <nahidhtamim@gmail.com>
2026-01-28 18:31:02 +0600

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -18,9 +18,33 @@ use App\Http\Controllers\Auth\DefaultAuthController;
 
 Route::get('stripe-connection/callback', [StripeController::class, 'stripeCallbackTwo'])->name('stripe.callback');
 Route::get('stripe-connection/success/{id}', function ($id) {
-    return redirect(rtrim(env('FRONTEND_URL'), '/') . "/stripe-connection/success/$id");
+    $frontendUrl = rtrim(env('FRONTEND_URL'), '/');
+    if (!$frontendUrl) {
+        return response('FRONTEND_URL is not set.', 500);
+    }
+    if (!preg_match('~^https?://~i', $frontendUrl)) {
+        $frontendUrl = 'https://' . $frontendUrl;
+    }
+    $frontendHost = parse_url($frontendUrl, PHP_URL_HOST);
+    if ($frontendHost && $frontendHost === request()->getHost()) {
+        return response('FRONTEND_URL points to the backend domain. Update it to the frontend domain.', 500);
+    }
+
+    return redirect($frontendUrl . "/stripe-connection/success/$id");
 });
 Route::get('stripe-connection/failed/{id}', function ($id) {
-    return redirect(rtrim(env('FRONTEND_URL'), '/') . "/stripe-connection/failed/$id");
+    $frontendUrl = rtrim(env('FRONTEND_URL'), '/');
+    if (!$frontendUrl) {
+        return response('FRONTEND_URL is not set.', 500);
+    }
+    if (!preg_match('~^https?://~i', $frontendUrl)) {
+        $frontendUrl = 'https://' . $frontendUrl;
+    }
+    $frontendHost = parse_url($frontendUrl, PHP_URL_HOST);
+    if ($frontendHost && $frontendHost === request()->getHost()) {
+        return response('FRONTEND_URL points to the backend domain. Update it to the frontend domain.', 500);
+    }
+
+    return redirect($frontendUrl . "/stripe-connection/failed/$id");
 });
 Route::get('/verify-email/{token}', [DefaultAuthController::class, 'verifyEmail']);
